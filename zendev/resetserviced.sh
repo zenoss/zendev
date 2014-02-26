f172d74b0a148aa6c6c78d3b43af60e36c0b0875
Phil Lu <plu@zenoss.com>
2014-02-26 17:02:11 -0600

    on slow VMs, wait for serviced to be ready before add-host

--- zendev/resetserviced.sh
+++ zendev/resetserviced.sh
@@ -7,7 +7,25 @@ EUROPA=$(zendev root)
 SERVICED=$(which serviced)
 sudo rm -rf /tmp/serviced*
 deploy () {
+    # give serviced a little time to startup
     sleep 20
+
+    # wait until serviced is ready - this is necessary on slow VMs
+    while true; do
+        if ! pgrep -fl "${SERVICED} -master" >/dev/null; then
+            echo "serviced is not running - will not add-host nor deploy-template"
+            return
+        fi
+        wget localhost:8787 &>/dev/null
+        if [[ 0 == $? ]]; then
+            echo "$(date +'%Y-%m-%d %H:%M:%S'): serviced is ready - performing add-host and deploy-template in 6 seconds"
+            sleep 6
+            break
+        fi
+        sleep 1
+    done
+
+    echo "$(date +'%Y-%m-%d %H:%M:%S'): performing add-host and deploy-template"
     ${SERVICED} add-host $IP:4979 default
     TEMPLATE_ID=$(${SERVICED} add-template ${EUROPA}/build/services/Zenoss)
     ${SERVICED} deploy-template ${TEMPLATE_ID} default zenoss
