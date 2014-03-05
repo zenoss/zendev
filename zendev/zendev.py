0b3f534faf54d7c148fd53d707119892cef8d367
Ian McCracken <ian.mccracken@gmail.com>
2014-03-05 14:20:32 -0600

    Add build targets for svcpkg

--- zendev/zendev.py
+++ zendev/zendev.py
@@ -332,7 +332,7 @@ def parse_args():
 
     build_parser = subparsers.add_parser('build')
     build_parser.add_argument('target', metavar='TARGET', 
-            choices=['src', 'core', 'resmgr'])
+            choices=['src', 'core', 'resmgr', 'svcpkg-core', 'svcpkg-resmgr', 'serviced'])
     build_parser.add_argument('-m', '--manifest', nargs="+",
             metavar='MANIFEST', required=False)
     build_parser.set_defaults(functor=build)
