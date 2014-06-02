983ce2ac2ea74abc1f72c3362dae3a33c0282791
Ian McCracken <ian.mccracken@gmail.com>
2014-06-02 11:15:41 -0500

    Fixing

--- boxes/ubuntu-14.04/scripts/dependencies.sh
+++ boxes/ubuntu-14.04/scripts/dependencies.sh
@@ -1,12 +1,19 @@
-apt-get -y install git vim-nox emacs24-nox htop wget python-dev python-pip mercurial bzr git libpam0g-dev tmux curl btrfs-tools
+apt-get -y install    \
+    git               \
+    vim-nox           \
+    emacs24-nox       \
+    htop              \
+    wget              \
+    python-dev        \
+    python-pip        \
+    mercurial         \
+    bzr               \
+    libpam0g-dev      \
+    tmux              \
+    curl              \
+    btrfs-tools       \
+    nfs-common        \
+    nfs-kernel-server \
+    golang
 
 pip install --upgrade pip
-
-# Download Go 1.2 and unpack it into /usr/local
-wget -qO- https://go.googlecode.com/files/go1.2.1.linux-amd64.tar.gz | tar -C /usr/local -xz
-
-# Set GOROOT and PATH appropriately
-cat <<EOF > /etc/profile.d/golang.sh
-    export GOROOT=/usr/local/go
-    export PATH=\$GOROOT/bin:\$PATH
-EOF
