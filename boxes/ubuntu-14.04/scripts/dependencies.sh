117e8a849004c9d4aebf72d9a33dca6a7872076e
Phil Lu <plu@zenoss.com>
2014-07-18 16:38:24 -0500

    replaced trusty-europa with ubuntu-14.04-europa

--- boxes/ubuntu-14.04/scripts/dependencies.sh
+++ boxes/ubuntu-14.04/scripts/dependencies.sh
@@ -10,10 +10,16 @@ apt-get -y install    \
     bzr               \
     libpam0g-dev      \
     tmux              \
+    screen            \
     curl              \
     btrfs-tools       \
     nfs-common        \
     nfs-kernel-server \
+    net-tools         \
     golang
+
+#wget http://artifacts.zenoss.loc/europa/docker-smuggle_2.24.2-1_amd64.deb
+#dpkg -i docker-smuggle_2.24.2-1_amd64.deb
+#apt-get install -f
 
 pip install --upgrade pip
