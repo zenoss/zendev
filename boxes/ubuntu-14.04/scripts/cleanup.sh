b3d7d34dc0d1c364bfff05f2b72925a66b94a31e
Phil Lu <plu@zenoss.com>
2014-07-19 08:47:47 -0500

    mounts not working - disabled cleanup of some dependencies

--- boxes/ubuntu-14.04/scripts/cleanup.sh
+++ boxes/ubuntu-14.04/scripts/cleanup.sh
@@ -5,22 +5,22 @@
 # * keeps the current kernel
 # * does not touch the virtual packages, e.g.'linux-image-generic', etc.
 #
-dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
+#dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
 
 # delete all linux headers
-dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
+#dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
 
 # delete linux source
-dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
+#dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
 
 # delete development packages
-dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+#dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
 
 # delete compilers and other development tools
-apt-get -y purge cpp gcc g++
+apt-get -y purge g++
 
 # delete X11 libraries
-apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
+#apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
 
 # delete obsolete networking
 apt-get -y purge ppp pppconfig pppoeconf
@@ -32,3 +32,6 @@ apt-get -y autoremove
 apt-get -y clean
 rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
 rm -f /tmp/chef*deb
+
+rm -fr util-linux-2.24.tar.bz2 util-linux-2.24
+
