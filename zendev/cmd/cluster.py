b784206848ee3e0d315e30721c9301012a4a4e4b
John Crocker <jcrocker@zenoss.com>
2014-09-11 10:53:14 -0500

    Move vdi creation to Vagrantfile
    Add --btrfs command line arg

--- zendev/cmd/cluster.py
+++ zendev/cmd/cluster.py
@@ -5,8 +5,7 @@ import py
 import subprocess
 from jinja2 import Template
 
-from box import BOXES, verify_auto_network, get_shared_directories, make_vdis
-
+from box import BOXES, verify_auto_network, get_shared_directories
 from ..utils import colored, here
 
 
@@ -14,8 +13,9 @@ VAGRANT = Template("""
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-$script = <<'SCRIPT'
-{{ provision_script }} 
+$script = <<SCRIPT
+{{ provision_script }}{%for i in range(vdis) %}
+{{"mkfs.btrfs -L volume.btrfs.%d /dev/sd%s"|format(i+1, "bcdef"[i])}} {%endfor%}
 SCRIPT
 
 Vagrant.configure("2") do |config|
@@ -24,26 +24,26 @@ Vagrant.configure("2") do |config|
       config.vm.box = "{{ box_name }}"
       config.vm.box_url = "http://vagrant.zendev.org/boxes/{{ box_name }}.box"
       config.vm.network :private_network, :ip => '0.0.0.0', :auto_network => true
-
       config.vm.hostname = vm_name
-
       config.vm.provider :virtualbox do |vb|
         vb.customize ["modifyvm", :id, "--memory", "{{ box_memory }}"]
-        vb.customize ["modifyvm", :id, "--cpus", 4]
-        {% set vdi_count = 1 %}
-        {% for vdi in vdis %}
-#TODO: vdis needs to handle vdi/box
-        vb.customize ["storageattach", :id, "--storagectl", "IDE Controller",
-                      "--port", {{ vdi_count }}, "--device", 0, "--type", "hdd", "--medium",
-                      "{{ vdi }}"]{% set vdi_count = vdi_count + 1 %}{% endfor %}
-      end
-
-      {% for root, target in shared_folders %}
+        vb.customize ["modifyvm", :id, "--cpus", 4]{% if vdis %}
+        (1..{{ vdis }}).each do |vol|
+          disc_file = "mnt/#{vm_name}/btrfs_#{vol}.vdi"
+          unless File.exist?(disc_file)
+            vb.customize ['createhd', '--filename', disc_file, '--size', 24 * 1024]
+          end
+          vb.customize ["storageattach", :id, "--storagectl", "IDE Controller",
+                        "--port", vol, "--device", 0, "--type", "hdd", "--medium",
+                        disc_file ]
+        end{% endif %}
+      end{% for root, target in shared_folders %}
       config.vm.synced_folder "{{ root }}", "{{ target }}"{% endfor %}
       {% if provision_script %}config.vm.provision "shell", inline: $script{% endif %}
     end
   end
 end
+
 """)
 
 PROVISION_SCRIPT = """
@@ -53,13 +53,11 @@ chown zenoss:zenoss /home/zenoss/%(env_name)s
 su - zenoss -c "cd /home/zenoss && zendev init %(env_name)s"
 echo "source $(zendev bootstrap)" >> /home/zenoss/.bashrc
 echo "zendev use %(env_name)s" >> /home/zenoss/.bashrc
-
 ln -sf /vagrant/etc_hosts /etc/hosts
 if ! $(grep -q ^$HOSTNAME /vagrant/etc_hosts 2>/dev/null) ; then
     IP=$(ifconfig eth1 | sed -n 's/^.*inet addr:\([^ ]*\).*/\\1/p')
     echo $HOSTNAME $IP >> /vagrant/etc_hosts
 fi
-%(formatDrive)s
 """
 
 ETC_HOSTS = """
@@ -80,30 +78,21 @@ class VagrantClusterManager(object):
         import vagrant
         return vagrant.Vagrant(self._root.join(name).strpath)
 
-    def create(self, name, purpose, count=1, memory=4096):
+    def create(self, name, purpose, count=1, btrfs=0, memory=4096):
         if not verify_auto_network():
             raise Exception("Unable to find or install vagrant-auto_network plugin.")
         elif self._root.join(name).check(dir=True):
             raise Exception("Vagrant box %s already exists" % name)
         vbox_dir = self._root.ensure(name, dir=True)
         shared = get_shared_directories(self.env)
-        vdis, formatDrive = [], []
-        for i in range(count):
-            relPath = "mnt/%s_%02d" % (self.env.name, i)
-            v,f = make_vdis(self._root, relPath, 0)
-            vdis.append(v)
-            formatDrive.append(f)
         params = dict(
             cluster_name=name,
             box_count=count,
             box_memory=memory,
             box_name=BOXES.get(purpose),
             shared_folders=shared,
-            vdis=vdis,  #TODO: vdis needs to handle vdi/box
-            provision_script=PROVISION_SCRIPT % {
-                'env_name': self.env.name,
-                'formatDrive': "\n".join(formatDrive)
-            }
+            vdis=btrfs,
+            provision_script=PROVISION_SCRIPT % {'env_name': self.env.name}
         )
         vbox_dir.ensure("etc_hosts").write(ETC_HOSTS)
         vbox_dir.ensure("Vagrantfile").write(VAGRANT.render(params))
@@ -151,10 +140,8 @@ class VagrantClusterManager(object):
 
 
 def cluster_create(args, check_env):
-    """
-    """
     env = check_env()
-    env.cluster.create(args.name, args.type, args.count, args.memory)
+    env.cluster.create(args.name, args.type, args.count, args.btrfs, args.memory)
     env.cluster.provision(args.name, args.type)
 
 
@@ -196,6 +183,8 @@ def add_commands(subparsers):
     cluster_create_parser.add_argument('--count', type=int, default=1)
     cluster_create_parser.add_argument('--memory', type=int, default=4096)
     cluster_create_parser.add_argument('--domain', default='zenoss.loc')
+    cluster_create_parser.add_argument('--btrfs', type=int, default=0,
+                                   help="Number of btrfs volumes")
     cluster_create_parser.set_defaults(functor=cluster_create)
 
     cluster_boot_parser = cluster_subparsers.add_parser('boot')
