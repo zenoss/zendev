51e65b294e8ee8b930020209bf35d2004aa9cc13
John Crocker <jcrocker@zenoss.com>
2015-03-10 10:12:03 -0500

    Use docker-1.5.0 and go-1.3.3 for CC-1.0

--- boxes/ubuntu-14.04-CC-1.0/scripts/golang-1.3.3.sh
+++ boxes/ubuntu-14.04-CC-1.0/scripts/golang-1.3.3.sh
@@ -1,4 +1,4 @@
-curl -s https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz | tar -xzC /usr/local
+curl -s https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz | tar -xzC /usr/local
 
 # Set GOROOT and PATH appropriately
 cat <<EOF | bash -c "cat > /etc/profile.d/golang.sh"
