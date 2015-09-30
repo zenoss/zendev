48da026aaaa0b6f4e97481791bfc6a0592e67a4e
Ian McCracken <ian.mccracken@gmail.com>
2015-09-30 10:40:13 -0500

    Don't push on sync

--- zendev/environment.py
+++ zendev/environment.py
@@ -376,9 +376,6 @@ class ZenDevEnvironment(object):
                 repo.checkout(repo.ref)
             repo.merge_from_remote()
         info("Remote changes have been merged")
-        for repo in self.repos(filter_):
-            info("Pushing %s" % repo.name)
-            repo.push()
         info("Up to date!")
 
     def use(self):
