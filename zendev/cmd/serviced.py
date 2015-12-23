2901ca8d1e75af6ad2a49eeec328f09af448e8a0
bwf93 <bfarrell@zenoss.com>
2015-12-23 10:14:34 -0600

    when attaching to a container set the term variable to something reasonable

--- zendev/cmd/serviced.py
+++ zendev/cmd/serviced.py
@@ -313,7 +313,7 @@ def run_serviced(args, env):
 
 
 def attach(args, env):
-    subprocess.call("serviced service attach '%s'; stty sane" % args.specifier, shell=True)
+    subprocess.call("serviced service attach '%s'  /bin/bash -c 'export TERM=xterm; exec bash'" % args.specifier, shell=True)
 
 
 def devshell(args, env):
