d5d77d22a0234702bbbb395f9006281c592f6bd4
Jason Peacock <jpeacock@zenoss.com>
2020-07-09 10:28:27 -0500

    Make zendev build work again.
    The build command has been updated to include the new zenservicemigration and mariadb-base build steps.
    
    Fixes ZEN-32929 and ZEN-32930.

--- zendev/cmd/build.py
+++ zendev/cmd/build.py
@@ -1,10 +1,7 @@
-import py
 import subprocess
 import sys
 
-from ..environment import ZenDevEnvironment, init_config_dir, NotInitialized
-from ..config import get_config
-from ..log import info, error
+from ..log import error, info
 
 
 def build(args, env):
@@ -21,20 +18,46 @@ def build(args, env):
     # of product-assembly which are NOT actually product directories
     zenpackManifestFile = targetDir.join("zenpacks.json")
     if not zenpackManifestFile.check():
-        error("Target product '%s' does not appear to be a valid product. Could not find %s" % (args.target_product, zenpackManifestFile.strpath))
+        error(
+            "Target product '%s' does not appear to be a valid product. "
+            "Could not find %s"
+            % (args.target_product, zenpackManifestFile.strpath),
+        )
         sys.exit(1)
 
-    productBase = env().productAssembly.join("product-base")
-    cmdArgs = ['make']
+    zenservicemigrations = env().productAssembly.join("svcdefs")
+    print "Building zenservicemigrations ..."
+    print "cd %s" % zenservicemigrations.strpath
+    zenservicemigrations.chdir()
+    cmdArgs = ["make"]
     if args.clean:
-        cmdArgs.append('clean')
-    cmdArgs.append('build')
+        cmdArgs.append("clean")
+    cmdArgs.append("migrations")
+    print " ".join(cmdArgs)
+    try:
+        subprocess.check_call(cmdArgs)
+    except Exception:
+        info("zenservicemigration build not found, skipping.")
 
+    cmdArgs = ["make"]
+    if args.clean:
+        cmdArgs.append("clean")
+    cmdArgs.append("build")
+
+    productBase = env().productAssembly.join("product-base")
     print "Building product-base ..."
     print "cd %s" % productBase.strpath
     productBase.chdir()
     print " ".join(cmdArgs)
     subprocess.check_call(cmdArgs)
+
+    mariadbBase = env().productAssembly.join("mariadb-base")
+    if mariadbBase.check():
+        print "Building mariadb-base ..."
+        print "cd %s" % mariadbBase.strpath
+        mariadbBase.chdir()
+        print " ".join(cmdArgs)
+        subprocess.check_call(cmdArgs)
 
     print "Building %s" % args.target_product
     print "cd %s" % targetDir.strpath
@@ -42,10 +65,19 @@ def build(args, env):
     print " ".join(cmdArgs)
     subprocess.check_call(cmdArgs)
 
+
 def add_commands(subparsers):
-    build_parser = subparsers.add_parser('build', help='Build Zenoss')
-    build_parser.add_argument('-c', '--clean', action="store_true", default=False,
-                              help='Delete any existing images before building')
-    build_parser.add_argument('target_product', metavar="TARGET",
-                               help='Name of the target product to build; e.g. core, resmgr, etc')
+    build_parser = subparsers.add_parser("build", help="Build Zenoss")
+    build_parser.add_argument(
+        "-c",
+        "--clean",
+        action="store_true",
+        default=False,
+        help="Delete any existing images before building",
+    )
+    build_parser.add_argument(
+        "target_product",
+        metavar="TARGET",
+        help="Name of the target product to build; e.g. core, resmgr, etc",
+    )
     build_parser.set_defaults(functor=build)
